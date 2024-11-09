107611af34598c2c376fe066c813557dd147cc03
jualga02 <jualga02@alumnatflorida.es>
2024-11-09 20:52:27 +0100

    Añadidos más animales en index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Lista de palabras para el juego
-$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance'];
+$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance','guepardo', 'pantera','leon','tigre'];
 
 // Inicializar el juego
 if (!isset($_SESSION['palabra'])) {
